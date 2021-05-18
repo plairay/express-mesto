771b2d261bae1d0043f6e3e061e03f9b0491b255
kostya <kostyanparrkuor@mail.ru>
2021-05-18 08:34:08 +0300

    review fix

--- app.js
+++ app.js
@@ -24,4 +24,12 @@ app.use((req, res, next) => {
 app.use('/users', usersRoutes);
 app.use('/', cardsRoutes);
 
+app.get('/*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
+app.post('/*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
+
 app.listen(PORT, () => {});
--- controllers/users.js
+++ controllers/users.js
@@ -64,8 +64,7 @@ exports.updateProfile = (req, res) => {
       }
     })
     .catch((err) => {
-      // eslint-disable-next-line no-constant-condition
-      if (err.name === 'CastError' || 'ValidationError') {
+      if (err.name === 'CastError' && 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные' });
       } else {
         res.status(500).send({ message: 'Произошла ошибка сервера' });
@@ -93,8 +92,7 @@ exports.updateAvatar = (req, res) => {
       }
     })
     .catch((err) => {
-      // eslint-disable-next-line no-constant-condition
-      if (err.name === 'CastError' || 'ValidationError') {
+      if (err.name === 'CastError' && 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные' });
       } else {
         res.status(500).send({ message: 'Произошла ошибка сервера' });
