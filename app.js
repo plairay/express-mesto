973049807ac33b6adfa5917316e230ad46c6fb5f
kostya <kostyanparrkuor@mail.ru>
2021-05-23 16:29:28 +0300

    review fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const { errors } = require('celebrate');
+const { celebrate, Joi, errors } = require('celebrate');
 const { usersRoutes } = require('./routes/users');
 const { cardsRoutes } = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
@@ -21,8 +21,22 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(2),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(2),
+  }),
+}), createUser);
 
 app.use('/users', Auth, usersRoutes);
 app.use('/cards', Auth, cardsRoutes);
